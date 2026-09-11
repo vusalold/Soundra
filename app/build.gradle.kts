32f90b180561b42b969b4cf08f2c9ce926dbba46
vusal <vusal.mzade@gmail.com>
2026-09-12 02:37:56 +0400

    Update: New app icon and SDK version fix to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.vusal.soundra"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
